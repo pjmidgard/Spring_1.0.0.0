c8f581076c41ccecd2d05e0611ff0571266c1af6
Java Software <pj.midgard@yahoo.com>
2021-10-03 19:24:59 +0100

    Update Spring_1.0.0.0.py

--- Spring_1.0.0.0.py
+++ Spring_1.0.0.0.py
@@ -591,3 +591,5 @@ class compression:
 d=compression()
 
 xw=d.cryptograpy_compression()
+
+print("x*n=y*n=z*n lenf_count_times=lenf2*(2**480) Bytes_row8=Fraction(Bytes_row1, lenf_count_times) m=math.floor(Bytes_row8) Bytes_row12=m*lenf_count_times Bytes_row14=Bytes_row1-Bytes_row12.Bytes_row16=Bytes_row1//m Bytes_row17=Bytes_row1-(Bytes_row16*m) Bytes_row2=Bytes_row1-(Bytes_row17-Bytes_row16*((Bytes_row8*lenf_count_times)+(Bytes_row8*lenf_count_times))*lenf_count_times)")
